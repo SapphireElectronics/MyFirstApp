0904c49628dc527ef13a3d22c13a0821f8f8cc84
Ashley Preston <ashley.cobalt@gmail.com>
2015-05-29 16:17:00 -0400

    Added menu bar

--- app/src/main/java/ca/sapphire/myfirstapp/MyActivity.java
+++ app/src/main/java/ca/sapphire/myfirstapp/MyActivity.java
@@ -17,6 +17,8 @@ public class MyActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -26,22 +28,7 @@ public class MyActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /** Called when the user clicks the Send button */
+     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
         // Do something in response to button
         Intent intent = new Intent( this, DisplayMessageActivity.class);
@@ -50,4 +37,19 @@ public class MyActivity extends ActionBarActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                //openSearch();
+                return true;
+            case R.id.action_settings:
+                //openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 }
